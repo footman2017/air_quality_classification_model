bf6866a1259124262dbc5e7d962d3f705a6e7b35
Raey Faldo <raey3221@gmail.com>
2023-08-25 19:56:33 +0700

    change subscriber using tabnet

--- deepLearning/CNN_hyperparameter2.py
+++ deepLearning/CNN_hyperparameter2.py
@@ -5,7 +5,9 @@ from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv1D, Flatten, MaxPooling1D
-from kerastuner.tuners import RandomSearch
+# from keras_tuner.tuners import RandomSearch
+from keras_tuner.tuners import BayesianOptimization
+
 
 # Load and preprocess data
 df = pd.read_csv('combined_data ver 1.1.csv')
@@ -31,54 +33,79 @@ y = to_categorical(y)
 
 def build_model(hp):
     model = Sequential()
-    model.add(Conv1D(filters=hp.Int('filters', min_value=16, max_value=128, step=16),
+    model.add(Conv1D(filters=hp.Int('filters', min_value=16, max_value=356, step=16),
                      kernel_size=hp.Int(
                          'kernel_size', min_value=1, max_value=5, step=1),
                      activation='relu',
                      input_shape=(6, 1)))
     model.add(MaxPooling1D(pool_size=2))
     model.add(Flatten())
-    model.add(Dense(units=hp.Int('dense_units', min_value=8, max_value=128, step=16),
+    model.add(Dense(units=hp.Int('dense_units', min_value=8, max_value=356, step=16),
                     activation='relu'))
     model.add(Dense(3, activation='softmax'))
-    model.compile(optimizer=hp.Choice('optimizer', ['adam', 'nadam', 'rmsprop']),
+    model.compile(optimizer=hp.Choice('optimizer', ['adam', 'nadam', 'rmsprop', 'sgd']),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
     return model
 
 
 # Setup Keras Tuner and hyperparameter search
-n_splits = 5
-kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
-best_models = []
-for fold, (train, test) in enumerate(kfold.split(X_cnn, np.argmax(y, axis=1))):
-    print(f"\n---- Hyperparameter Tuning on Fold {fold + 1} ----")
+# n_splits = 5
+# kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+# best_models = []
+# for fold, (train, test) in enumerate(kfold.split(X_cnn, np.argmax(y, axis=1))):
+#     print(f"\n---- Hyperparameter Tuning on Fold {fold + 1} ----")
 
-    tuner = RandomSearch(
-        build_model,
-        objective='val_accuracy',
-        max_trials=5,
-        executions_per_trial=3,
-        directory='output',
-        project_name=f'AirQualityCNN_Fold{fold + 1}'
-    )
+#     tuner = RandomSearch(
+#         build_model,
+#         objective='val_accuracy',
+#         max_trials=20,
+#         executions_per_trial=3,
+#         directory='output',
+#         project_name=f'AirQualityCNN_Fold{fold + 1}'
+#     )
 
-    X_train, X_test = X_cnn[train], X_cnn[test]
-    y_train, y_test = y[train], y[test]
+#     X_train, X_test = X_cnn[train], X_cnn[test]
+#     y_train, y_test = y[train], y[test]
 
-    tuner.search(X_train, y_train, epochs=5, validation_data=(X_test, y_test), verbose=0)
+#     tuner.search(X_train, y_train, epochs=100, validation_data=(X_test, y_test), verbose=0)
 
-    # Display best hyperparameters
-    best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
-    print(f"""
-    Best hyperparameters for fold {fold + 1}:
-    Filters: {best_hps.get('filters')}
-    Kernel Size: {best_hps.get('kernel_size')}
-    Dense Units: {best_hps.get('dense_units')}
-    Optimizer: {best_hps.get('optimizer')}
-    """)
+#     # Display best hyperparameters
+#     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+#     print(f"""
+#     Best hyperparameters for fold {fold + 1}:
+#     Filters: {best_hps.get('filters')}
+#     Kernel Size: {best_hps.get('kernel_size')}
+#     Dense Units: {best_hps.get('dense_units')}
+#     Optimizer: {best_hps.get('optimizer')}
+#     """)
+
+tuner = BayesianOptimization(
+    build_model,
+    objective='val_accuracy',
+    max_trials=20,
+    seed=42,
+    directory='output',
+    project_name=f'AirQualityCNN'
+)
+
+split_idx = int(0.8 * len(X_cnn))
+X_train, X_test = X_cnn[:split_idx], X_cnn[split_idx:]
+y_train, y_test = y[:split_idx], y[split_idx:]
+
+tuner.search(X_train, y_train, epochs=100, validation_data=(X_test, y_test), verbose=1)
+
+# Display best hyperparameters
+best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+print(f"""
+Best hyperparameters:
+Filters: {best_hps.get('filters')}
+Kernel Size: {best_hps.get('kernel_size')}
+Dense Units: {best_hps.get('dense_units')}
+Optimizer: {best_hps.get('optimizer')}
+""")
 
     # You can continue to train the best model for more epochs or save it if you want.
-    best_model = tuner.hypermodel.build(best_hps)
-    best_model.fit(X_train, y_train, epochs=100, batch_size=10, validation_data=(X_test, y_test), verbose=0)
-    best_models.append(best_model)
+    # best_model = tuner.hypermodel.build(best_hps)
+    # best_model.fit(X_train, y_train, epochs=100, batch_size=10, validation_data=(X_test, y_test), verbose=0)
+    # best_models.append(best_model)
--- mqtt/subscriber.py
+++ mqtt/subscriber.py
@@ -3,37 +3,97 @@ import json
 import tensorflow as tf
 import requests
 import numpy as np
+from pytorch_tabnet.tab_model import TabNetClassifier
+import joblib
 
 KATEGORI = {0: 'BAIK', 1: 'SEDANG', 2: 'TIDAK BAIK'}
 
+# Load model Keras
+# Ganti dengan lokasi dan nama model Anda
+# model = tf.keras.models.load_model('trained_model_lstm.h5')
+
+
+# def predict_with_model(json_data):
+#     # Ubah data JSON menjadi bentuk yang sesuai dengan model Anda
+#     # Sebagai contoh, model ini mengharapkan input berupa numpy array dengan bentuk (1, 6)
+#     input_data = [[json_data["pm10"], json_data["pm25"], json_data["so2"],
+#                    json_data["co"], json_data["o3"], json_data["no2"]]]
+
+#     # Lakukan prediksi
+#     prediction = model.predict(input_data)
+#     highest_prob = np.max(prediction)
+#     highest_prob_index = np.argmax(prediction)
+#     predicted_class_label = KATEGORI[highest_prob_index]
+
+#     return {
+#         'accuracy': round((highest_prob * 100), 2),
+#         # 'category': predicted_class_label
+#         'category': highest_prob_index
+#     }
+
+# Load TabNet model
+# It seems you've already loaded the model into loaded_clf
+loaded_clf = TabNetClassifier()
+loaded_clf.load_model("./tabnet_model_full_dataset.zip")
+scaler = joblib.load('scaler.pkl')
+
 
 def predict_with_model(json_data):
-    # Ubah data JSON menjadi bentuk yang sesuai dengan model Anda
-    # Sebagai contoh, model ini mengharapkan input berupa numpy array dengan bentuk (1, 6)
-    input_data = [[json_data["pm10"], json_data["pm25"], json_data["so2"],
-                   json_data["co"], json_data["o3"], json_data["no2"]]]
+    # Convert JSON data to a format suitable for your model
+    input_data = np.array([[json_data["pm10"], json_data["pm25"], json_data["so2"],
+                            json_data["co"], json_data["o3"], json_data["no2"]]])
 
-    # Load model Keras
-    # Ganti dengan lokasi dan nama model Anda
-    model = tf.keras.models.load_model('trained_model_lstm.h5')
+    # Preprocess the data: normalization, etc. Here, assuming you still have the 'scaler' object available from above.
+    input_data = scaler.transform(input_data)
 
-    # Lakukan prediksi
-    prediction = model.predict(input_data)
-    highest_prob = np.max(prediction)
-    highest_prob_index = np.argmax(prediction)
+    # Make a prediction with the TabNet model
+    probabilities = loaded_clf.predict_proba(input_data)
+    highest_prob_index = np.argmax(probabilities)
+    highest_prob = probabilities[0][highest_prob_index]
+
     predicted_class_label = KATEGORI[highest_prob_index]
 
     return {
-        'accuracy': highest_prob,
-        'category': predicted_class_label
+        'accuracy': round((highest_prob * 100), 2),
+        'category': highest_prob_index,
+        'category_name': predicted_class_label,
     }
 
-def on_message(client, userdata, message):      # Fungsi untuk melakukan subscribe ke topik MQTT
+
+def send_to_be(json_data, prediction_result):
+    url = "http://127.0.0.1:8000/api/store-prediction"
+
+    payload = json.dumps({
+        "pm10": json_data["pm10"],
+        "pm25": json_data["pm25"],
+        "so2": json_data["so2"],
+        "co": json_data["co"],
+        "o3": json_data["o3"],
+        "no2": json_data["no2"],
+        "prediction_result": int(prediction_result['category']),
+        "accuracy": prediction_result['accuracy']
+    })
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+# Fungsi untuk melakukan subscribe ke topik MQTT
+
+
+def on_message(client, userdata, message):
     payload = message.payload.decode('utf-8')
     data_json = json.loads(payload)
 
     # Lakukan prediksi dengan model
     result = predict_with_model(data_json)
+
+    # simpan ke be
+    response = send_to_be(data_json, result)
 
     # Lakukan sesuatu dengan hasil prediksi, misalnya mencetaknya
     print("Hasil prediksi:", result)
@@ -49,16 +109,5 @@ client.connect(broker_address, port)
 topic = "air_parameter"  # Ganti dengan nama topik yang Anda gunakan
 client.subscribe(topic)
 client.on_message = on_message
-
+print('Subscriber ready')
 client.loop_forever()
-
-# data_json = {
-#     "pm10": 30,
-#     "pm25": 66,
-#     "so2": 9,
-#     "co": 13,
-#     "o3": 1,
-#     "no2": 7
-# }
-
-# print(predict_with_model(data_json))
